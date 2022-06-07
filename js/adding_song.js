4782af00001ec36114b6c352acb850d32a299aab
Amar3298 <79484860+Amar3298@users.noreply.github.com>
2022-06-07 14:54:15 +0530

    change of color according to chada

--- js/adding_song.js
+++ js/adding_song.js
@@ -1,4 +1,4 @@
-let audioElement = new Audio("./audio/song1.mp3");
+let audioElement = new Audio("./audio/song3.mp3");
 let masterplay = document.getElementById("castle");
 masterplay.addEventListener("click", () => {
   if (audioElement.paused || audioElement.currentTime <= 0) {
@@ -8,12 +8,12 @@ masterplay.addEventListener("click", () => {
   }
 });
 
-let full_song = new Audio("./audio/song3.mp3");
-let playbutton = document.getElementById("song");
-playbutton.addEventListener("click", () => {
-  if (full_song.paused || full_song.currentTime <= 0) {
-    full_song.play();
-  } else {
-    full_song.pause();
-  }
-});
+// let full_song = new Audio("./audio/song3.mp3");
+// let playbutton = document.getElementById("song");
+// playbutton.addEventListener("click", () => {
+//   if (full_song.paused || full_song.currentTime <= 0) {
+//     full_song.play();
+//   } else {
+//     full_song.pause();
+//   }
+// });
